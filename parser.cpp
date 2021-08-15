ce0368d2858c3b5e76812c7b0a26858266ea9549
Alexander Jung <onebit74@gmail.com>
2021-08-15 22:24:52 +0200

    .

--- parser.cpp
+++ parser.cpp
@@ -13,11 +13,13 @@ public:
 		return pos == input.size();
 	}
 	void indent(){
-			for(size_t i = 0; i < depth; ++i)std::cout << '\t';
+		for(size_t i = 0; i < depth; ++i)std::cout << '\t';
 	}
 };
 class parse_object {
 public:
+	std::string comment;
+	bool dbg_inline;
 	size_t start_match;
 	virtual ~parse_object() = default;
 	virtual bool _match(context&) {return false;}
@@ -41,15 +43,28 @@ public:
 		}
 	}
 	bool match(context& ctx){
-		if(ctx.debug){
+		if(ctx.debug && !dbg_inline){
 			ctx.indent();
-			std::cout << "starting" << std::endl;
-		}
-		ctx.depth++;
-		auto prev_pos = ctx.pos;
-		auto ret = _match(ctx);
-		ctx.depth--;
-		if(ctx.debug){
+			std::cout << "starting ";
+			constexpr int lo = 7;
+			constexpr size_t ro = 7;
+			auto start_pos = std::max(0,int(ctx.pos)-lo);
+			auto end_pos = std::min(ctx.input.size(),ctx.pos+ro);
+			std::cout << '\"';
+			std::cout << std::string_view{ctx.input.begin()+start_pos, ctx.input.begin()+end_pos};
+			std::cout << '\"';
+			std::cout << " " << comment;
+			std::cout << std::endl;
+			ctx.indent();
+			std::cout << "          ";
+			for(int i = start_pos; i <= end_pos; ++i){
+				std::cout << (i == ctx.pos ? '^' : ' ');
+			}
+			std::cout << std::endl;
+			ctx.depth++;
+			auto prev_pos = ctx.pos;
+			auto ret = _match(ctx);
+			ctx.depth--;
 			ctx.indent();
 			if(!ret){
 				std::cout << "failed ";
@@ -57,8 +72,10 @@ public:
 				std::cout << "accepted ";
 			}
 			std::cout << '\"' << std::string_view{ctx.input.begin()+prev_pos,ctx.input.begin()+ctx.pos} <<'\"' << std::endl;
+			return ret;
+		}else{
+			return _match(ctx);
 		}
-		return ret;
 	}
 	void undo(context& ctx){
 		_undo(ctx);
@@ -82,18 +99,111 @@ public:
 		return impl->match_or_undo(ctx);
 	}
 	bool operator()(std::string_view sv){
-		(*impl)(sv);
+		return (*impl)(sv);
 	}
-	void operator()(context& ctx){
-		(*impl)(ctx);
+	bool operator()(context& ctx){
+		return (*impl)(ctx);
 	}
 	bool match(context& ctx){
 		return impl->match(ctx);
 	}
 	std::shared_ptr<parse_object> impl;
 };
-parse_object_ref f_parser(std::function<bool(context& ctx)> f){
-	return {std::make_shared<f_parser_t>(f)};
+parse_object_ref f_parser(std::function<bool(context& ctx)> f, bool dbg_inline = false, std::string comment = ""){
+	auto ret = parse_object_ref{std::make_shared<f_parser_t>(f)};
+	ret.impl->dbg_inline = dbg_inline;
+	ret.impl->comment = comment;
+	return ret;
+}
+struct print_t {
+	parse_object_ref operator()(std::string_view text){
+		return f_parser([=](auto){
+					std::cout << text << std::endl;
+					return true;
+				});
+	}
+} print;
+template<typename FIRST = void, typename...ARGS>
+class output_wraper{
+	std::function<void(FIRST,ARGS...)> destination;
+	public:
+	output_wraper(auto dest) : destination(dest) {};
+	output_wraper<ARGS...> operator<<(FIRST& val){
+		return *this << std::move(val);
+	}
+	output_wraper<ARGS...> operator<<(FIRST&& val){
+		return output_wraper<ARGS...>{[cb = destination,val = std::move(val)]
+			(ARGS...args){
+				cb(val,args...);
+			}};
+	}
+
+};
+template<>
+class output_wraper<void> {
+	std::function<void()> destination;
+	public:
+	output_wraper(auto dest) : destination(dest) {};
+	~output_wraper(){
+		if(destination){
+			destination();
+		}else{
+			std::cout << "no dest";
+		}
+	}
+};
+template<typename...ARGS>
+class ret_parse_object : public parse_object_ref {
+	public:
+	std::function<bool(context&, output_wraper<ARGS...>)> inner;
+	std::function<void(ARGS...)> dest;
+	ret_parse_object operator>>(print_t){
+
+		auto ret = ret_parse_object{inner};
+		ret.dest = [](ARGS...args){
+			((std::cout << std::forward<ARGS>(args)),...);
+			std::cout << std::endl;
+		};
+		return ret;
+	}
+	ret_parse_object operator>>(std::function<void(ARGS...)> dest){
+		auto ret = ret_parse_object{inner};
+		ret.dest = dest;
+		return ret;
+	}
+	ret_parse_object(std::function<bool(context&,output_wraper<ARGS...>&&)> inner) : 
+		parse_object_ref(
+			f_parser([&](context& ctx){
+				if(!dest){
+					dest = [](ARGS...){};
+				}
+				return this->inner(ctx,output_wraper<ARGS...>{dest});
+			})),
+		inner(inner) 
+	{
+	}
+};
+template<typename...ARGS>
+auto r_parser(std::invocable<context&,output_wraper<ARGS...>&&> auto in_f){
+	std::function<bool(context&,output_wraper<ARGS...>&&)> f = in_f;
+	return ret_parse_object<ARGS...>{f};
+}
+auto r_parser(std::invocable<context&> auto in_f){
+	std::function<bool(context&)> f = in_f;
+	return r_parser<std::string_view>([=](context& ctx,output_wraper<std::string_view>&& output) -> bool{
+		int start_pos = ctx.pos;
+		auto success = f(ctx);
+		if(success){
+			output << std::string_view{ctx.input.begin()+start_pos,ctx.input.begin()+ctx.pos};
+		}
+		return success;
+	});
+}
+template<typename T>
+auto assign(T& target){
+	return [&](T&& t){
+		target = std::forward<T>(t);
+	};
 }
 parse_object_ref text_parser(std::string_view sv){
 	return f_parser([=]
@@ -112,40 +222,20 @@ parse_object_ref operator+(parse_object_ref lhs, parse_object_ref rhs){
 	return f_parser([=] 
 				(context& ctx) mutable {
 					return lhs.match(ctx) && rhs.match(ctx);
-				});
+				},true);
 }
 parse_object_ref operator+(std::string_view sv, parse_object_ref rhs){
-	return 
-		f_parser([=]
-			(context& ctx){
-				for(char c : sv){
-					if(ctx.done())return false;
-					if(ctx.get() != c)return false;
-					++ctx.pos;
-				}
-				return true;
-			}
-		) + rhs;
+	return text_parser(sv) + rhs;
 		
 }
 parse_object_ref operator+(parse_object_ref lhs, std::string_view sv){
-	return lhs + 
-		f_parser([=]
-			(context& ctx){
-				for(char c : sv){
-					if(ctx.done())return false;
-					if(ctx.get() != c)return false;
-					++ctx.pos;
-				}
-				return true;
-			}
-		);
+	return lhs + text_parser(sv);
 }
 parse_object_ref operator|(parse_object_ref lhs, parse_object_ref rhs){
 	return f_parser([=] 
 				(context& ctx) mutable {
 					return lhs.match_or_undo(ctx) || rhs.match(ctx);
-				});
+				},true);
 }
 parse_object_ref operator|(parse_object_ref lhs, std::string_view rhs){
 	return lhs | text_parser(rhs);
@@ -183,21 +273,13 @@ parse_object_ref ws(std::make_shared<ws_t>());
 /* 		return ret; */
 /* 	} */
 /* } */
-struct print_t {
-	parse_object_ref operator()(std::string_view text){
-		return f_parser([=](auto){
-					std::cout << text << std::endl;
-					return true;
-				});
-	}
-} print;
 class optional_t {} optional;
 class any_t {} any;
-parse_object_ref operator>>(any_t,parse_object_ref rhs){
-	return f_parser([=](context& ctx) mutable {
-			while(rhs.match_or_undo(ctx) && !ctx.done()){}
-			return true;
-		});
+auto operator>>(any_t,parse_object_ref rhs){
+	return r_parser([=](context& ctx) mutable {
+		while(rhs.match_or_undo(ctx) && !ctx.done()){}
+		return true;
+	});
 }
 parse_object_ref operator>>(any_t,std::string_view rhs){
 	return any >> text_parser(rhs);
@@ -237,7 +319,7 @@ parse_object_ref digit = f_parser([](context& ctx){
 	}
 	return false;
 });
-auto letter = f_parser([](context& ctx){
+auto graph_letter = r_parser([](context& ctx){
 	if(ctx.done())return false;
 	if(std::isgraph(ctx.get())){
 		++ctx.pos;
@@ -261,70 +343,6 @@ auto string = "\"" + (any >> (not_v >> "\"")) + "\"";
 /* 		}; */
 /* 	} */
 /* }; */
-template<typename FIRST = void, typename...ARGS>
-class output_wraper{
-	std::function<void(FIRST,ARGS...)> destination;
-	public:
-	output_wraper(auto dest) : destination(dest) {};
-	output_wraper<ARGS...> operator<<(FIRST& val){
-		return *this << std::move(val);
-	}
-	output_wraper<ARGS...> operator<<(FIRST&& val){
-		return output_wraper<ARGS...>{[cb = destination,val = std::move(val)]
-			(ARGS...args){
-				cb(val,args...);
-			}};
-	}
-
-};
-template<>
-class output_wraper<void> {
-	std::function<void()> destination;
-	public:
-	output_wraper(auto dest) : destination(dest) {};
-	~output_wraper(){
-		if(destination){
-			destination();
-		}else{
-			std::cout << "no dest";
-		}
-	}
-};
-template<typename...ARGS>
-auto r_parser(auto in_f){
-	std::function<bool(context&,output_wraper<ARGS...>&&)> f = in_f;
-	class ret_parse_object : public parse_object_ref {
-		public:
-		std::function<bool(context&, output_wraper<ARGS...>)> inner;
-		std::function<void(ARGS...)> dest;
-		ret_parse_object operator>>(print_t){
-
-			auto ret = ret_parse_object{inner};
-			ret.dest = [](ARGS...args){
-				((std::cout << std::forward<ARGS>(args)),...);
-				std::cout << std::endl;
-			};
-			return ret;
-		}
-		ret_parse_object operator>>(std::function<void(ARGS...)> dest){
-			auto ret = ret_parse_object{inner};
-			ret.dest = dest;
-			return ret;
-		}
-		ret_parse_object(std::function<bool(context&,output_wraper<ARGS...>&&)> inner) : 
-			parse_object_ref(
-				f_parser([&](context& ctx){
-					if(!dest){
-						dest = [](ARGS...){};
-					}
-					return this->inner(ctx,output_wraper<ARGS...>{dest});
-				})),
-			inner(inner) 
-		{
-		}
-	};
-	return ret_parse_object{f};
-}
 /* auto number = (optional >> "+" | "-") + (star >> digit); */
 template<typename integer, int base>
 auto number = r_parser<integer>([](context& ctx, auto&& output){
@@ -420,7 +438,25 @@ auto minmax(int val1, int val2){
 auto exact(int val){
 	return minmax(val,val);
 }
-auto re_match(std::string_view pattern){
+auto until(std::string_view pattern){
+	return r_parser<std::string_view>([=](auto& ctx,auto&& output){
+		const std::string str{pattern};
+		auto [regex_iter,b] = ctx.regex_cache.try_emplace(str,str);
+		std::smatch match;
+		const auto& c_input = ctx.input;
+		std::regex_search(c_input.begin()+ctx.pos,c_input.end(),match,regex_iter->second);
+		if(match.empty())return false;
+		ctx.pos += match.position();
+		auto ret = std::string_view{
+			c_input.begin() + ctx.pos,
+			c_input.begin() + ctx.pos + match.length()
+		};
+		ctx.pos += match.length();
+		output << ret;
+		return true;
+	});
+}
+auto match(std::string_view pattern){
 	return r_parser<std::string_view>([=](auto& ctx,auto&& output){
 		const std::string str{pattern};
 		auto [regex_iter,b] = ctx.regex_cache.try_emplace(str,str);
@@ -438,6 +474,15 @@ auto re_match(std::string_view pattern){
 		return true;
 	});
 }
+auto text(std::string_view& sv){
+	return f_parser([&](context& ctx){
+		for(int i = 0; i < sv.size(); ++i){
+			if(sv[i] != ctx.get())return false;
+			++ctx.pos;
+		}
+		return true;
+	});
+}
 int main(){
 	context ctx;
 	/* ctx.input = "  hey  "; */
@@ -446,7 +491,7 @@ int main(){
 	/* (((any >> "hey" + print("itter")) + eoi + print("done")) | print("fail"))(ctx); */
 	/* ctx.input="\"hey you rock\""; */
 	/* (string + print("accept") | print("fail"))(ctx); */
-	ctx.input = "-2342, 4";
+	/* ctx.input = "-2342, 4"; */
 	/* ((number ) + eoi + print("accept"))(ctx); */
 	/* ((r_parser<int>([](context&,output_wraper<int>&& output)->bool{ */
 	/* 	output << 2; */
@@ -456,7 +501,7 @@ int main(){
 	//1.output
 	//2.concatenation of parse_obj
 	//3.modifiers
-	(any >> (decimal<int> >> print) + ", ")(ctx);
+	/* (any >> (decimal<int> >> print) + ", ")(ctx); */
 	/* std::vector<int> res; */
 	/* (decimal<int> >> push(res))(ctx); */
 	/* ((decimal<int> >> push(res))+", "+(decimal<int> >> print))(ctx); */
@@ -467,5 +512,29 @@ int main(){
 	/* ctx.input = "bb"; */
 	/* ((optional>>"a") + print("accepted") | print("failed"))(ctx); */
 	/* (((re_match("ab*c") >> print) + print("accepted")) | print("failed"))("abbdbc"); */
+
+	//output composition decimal<int> + ", "+ decimal<int> >> print >> print
 	
+
+	struct node {
+		std::string_view name;
+		std::vector<node> children;
+		std::string_view text_content;
+	};
+	ret_parse_object<node> xml_node = r_parser<node>([&](auto& ctx,auto&& output){
+		node ret;
+		if((
+			"<"+ws+(match("\\w+") >> assign(ret.name)) + ws +">"+
+			(
+				(any >> (xml_node >> push(ret.children))) |
+				(until("</") >> assign(ret.text_content))) +
+			"</"+ws+text(ret.name)+ws+">")(ctx)){
+
+			output << ret;
+			return true;
+		}
+		return false;
+	});
+	node root;
+	(xml_node >> assign(root))("<hey></hey>");
 }
