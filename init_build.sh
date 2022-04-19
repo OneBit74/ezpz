2132a9ff52f277a9073b7a15c8d5511321f66c0c
Alexander Jung <alexander.jung999@gmail.com>
2022-04-19 10:30:05 +0200

    added compile_commands.json link

--- init_build.sh
+++ init_build.sh
@@ -3,8 +3,8 @@ mkdir -p build
 mkdir -p build/release
 mkdir -p build/debug
 cd build/release
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release ../..
+cmake ../.. -DCMAKE_BUILD_TYPE=Release -GNinja
 	# -DCMAKE_CXX_COMPILER:FILEPATH=clang++
 cd ../debug
-cmake -GNinja -DCMAKE_BUILD_TYPE=Debug ../..
+cmake ../.. -DCMAKE_BUILD_TYPE=Debug -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 	# -DCMAKE_CXX_COMPILER:FILEPATH=clang++
